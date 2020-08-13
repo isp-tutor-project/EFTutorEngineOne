6ef1ef984989ed2ca8c0fe22da184ff4a94984a1
Scott Silliman <scott.t.silliman@gmail.com>
2020-08-13 14:30:47 -0400

    completely reworked to let ispAPI do all of the low-level stuff

--- src/network/WebLogger.ts
+++ src/network/WebLogger.ts
@@ -10,68 +10,91 @@ export class WebLogger {
     public currFeatures:string;
     public currTutorNdx:number;
     public currTutorState:string;
-    
+
     /*public contructor(currScene:any, currFeatures:string) {
         this.currScene = currScene;
         this.currFeatures = currFeatures;
     }*/
     //private firebase:any;
     public constructor() {
-        this.currScene = this.getCurrentScene();
-        this.currFeatures = "FTR_WEB:FTR_CHOICE";   
+        this.currScene = null;
+        this.currFeatures = "";
     }
 
     public setValues(currScene:any, currFeatures:string) {
+        // console.log(`setting currFeatures to: ${currFeatures}`)
         this.currScene = currScene;
         this.currFeatures = currFeatures;
+        if (currFeatures !== ispAPI.getFeaturesString()) {
+            console.error("features are different");
+        } else {
+            console.log("features are the same");
+        }
     }
-    
-    public updateTutorState(tutorID:string, tutorStateJSON:string) {
-        console.log("LJSCR update Tutor State: ");
 
-        //console.log(tutorStateJSON);
-/*
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", 'http://localhost:3000/printstuff', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(tutorStateJSON);
-*/
-        /*db.collection(collectionID).doc(this.getUserId()).set({
-            currTutorNdx: this.currTutorNdx,
-            data: tutorStateJSON
-        })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error:any) {
-            console.error("Error writing document: ", error);
-        });*/
-
-        this.currTutorState = tutorStateJSON;
-
+    public getUserId() {
+        // let userid = userID;
+        // console.log("WebLogger::getUserId()", userid);
+        return ispAPI.getUserID();
     }
-    
+
+    public getCurrentScene() {
+        console.log("WebLogger.getCurrentScene()", this.currScene);
+        return this.currScene;
+    }
+
+    public getFeatures() {
+        // console.log("WebLogger::getFeatures(): ", this.currFeatures);
+        return ispAPI.getFeaturesString();
+    }
+
     public async getTutorState(tutorID:string):Promise<string> {
-        let jsonData = "";
-        let docRef = db.collection(collectionID).doc(this.getUserId());
-        await docRef.get().then((doc:any) => {
-            if (doc.exists) {
-                console.log("Document exists");
-                jsonData = doc.data().rqted;
-                this.currTutorNdx = doc.data().currTutorNdx;
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch(function(error:any) {
-            console.log("Error getting document:", error);
-        });
-        //console.log("returned data: "+ jsonData);
-        return jsonData;
+        console.log(`WebLogger::getTutorState("${tutorID}")`);
+        return ispAPI.getAppData(false);
+        // let jsonData = "";
+        // let docRef = db.collection(collectionID).doc(this.getUserId());
+        // await docRef.get().then((doc:any) => {
+        //     if (doc.exists) {
+        //         let feats = this.getFeatures().split(":");
+        //         console.log(feats);
+        //         console.log("WebLogger::Document exists");
+        //         jsonData = doc.data().rqted;
+        //         let obj = JSON.parse(jsonData);
+        //         console.log(obj.fFeatures);
+        //         let missingFeats = false;
+        //         for (let feat of feats) {
+        //             console.log(`testing for feature: ${feat}`);
+        //             if (feat in obj.fFeatures) {
+        //                 console.log(obj.fFeatures[feat]);
+        //             } else {
+        //                 console.log("nope");
+        //                 missingFeats = true;
+        //                 break;
+        //             }
+        //         }
+        //         if (missingFeats) {
+        //             console.log("feats mismatch. skipping");
+        //             jsonData = "";
+        //         }
+        //         // console.log("returned data: ", jsonData);
+        //         //JSON.stringify(json, null, 4));
+        //         this.currTutorNdx = doc.data().currTutorNdx;
+        //     } else {
+        //         // doc.data() will be undefined in this case
+        //         console.log("WebLogger::No such document!");
+        //     }
+        // }).catch(function(error:any) {
+        //     console.log("Error getting document:", error);
+        // });
+        // return jsonData;
     }
-    
+
     public logState(scenename:string, scene:string, module:string, tutor:string) {
-        console.log("LJSCR logState: ");
+        console.log("WebLogger::logState()");
+        console.log("scenename", scenename);
+        console.log("scene", scene);
+        console.log("module", module);
+        console.log("tutor", tutor);
 
         let buffer:string = "{\"scene\":" + scene + "," + "\"module\":" + module + "," + "\"tutor\":" + tutor + "}";
 /*
@@ -85,75 +108,75 @@ export class WebLogger {
             Log.e(TAG, "Log Write Failed : " + e);
         }*/
     }
-    
-    public getUserId() {
-        let userid = userID;
-        console.log("LJSCR get User ID: " + userid);
-        return userid;
-    }
-    
-    public getCurrentScene() {
-        // this is sync request
-        /*var xhr = new XMLHttpRequest();
-        let jsonData = "";
-        xhr.onreadystatechange = function() {
-            //console.log(xhr.responseText);
-            if (this.readyState == 4 && this.status == 200) {
-                //alert(xhr.responseText);
-                console.log("successfully got tutor state," + "readyState:"+this.readyState + ", status:" + this.status)
-                jsonData = xhr.responseText;
-                
-            }
-           
-        }
-        xhr.open('GET', 'http://localhost:3000/getCurrScene', false);
-        xhr.send();
-        return jsonData;*/
-        return this.currScene;
-    }
-    
-    public getFeatures() {
-        console.log("LJSCR getFeatures: ");
-        return this.currFeatures;
-    }
-    
-    public tutorComplete() {
-        this.currTutorNdx++;
-        db.collection(collectionID).doc(this.getUserId()).set({
+
+
+    public updateTutorState(tutorID: string, tutorStateJSON: string) {
+        console.log(`WebLogger::updateTutorState("${tutorID}")`);
+        // console.log(tutorStateJSON);
+        /*
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", 'http://localhost:3000/printstuff', true);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.send(tutorStateJSON);
+        */
+        /*db.collection(collectionID).doc(this.getUserId()).set({
             currTutorNdx: this.currTutorNdx,
-            rqted: this.currTutorState
+            data: tutorStateJSON
         })
         .then(function() {
             console.log("Document successfully written!");
-            console.log("LJSCR Tutor Complete: ");
-            //alert("tutor complete");
-            document.getElementById("completion-overlay").style.display = "block";
         })
         .catch(function(error:any) {
             console.error("Error writing document: ", error);
-        });
+        });*/
+        this.currTutorState = tutorStateJSON;
+        ispAPI.saveAppData(this.currTutorState)
+        .then(() => console.log("WebLogger::state persisted to db"));
+    }
 
-        
+
+    public tutorComplete() {
+        console.log("WebLogger::tutorComplete()");
+        ispAPI.saveAppData(this.currTutorState)
+        .then(() => ispAPI.saveAppComplete())
+        .then(() => ispAPI.goHomePage())
+        .catch((error: any) => console.error(error));
+        // this.currTutorNdx++;
+        // db.collection(collectionID).doc(this.getUserId()).set({
+        //     currTutorNdx: this.currTutorNdx,
+        //     rqted: this.currTutorState
+        // })
+        // .then(function() {
+        //     console.log("Document successfully written!");
+        //     console.log("LJSCR Tutor Complete: ");
+        //     //alert("tutor complete");
+        //     document.getElementById("completion-overlay").style.display = "block";
+        // })
+        // .catch(function(error:any) {
+        //     console.error("Error writing document: ", error);
+        // });
+
+
         // i can do something cool here
         //window.location.href = 'https://www.google.com';
     }
-    
+
     public updateScene(sceneName: string, sceneid: string) {
-        console.log("LJSCR Updating Current Scene: " + sceneid);
+        console.log(`WebLogger::updateScene("${sceneName}", "${sceneid}")`);
         this.currScene = sceneid;
-        /*var xhr = new XMLHttpRequest();
+        /*
+        var xhr = new XMLHttpRequest();
         xhr.open("POST", 'http://localhost:3000/currScene', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({currScene: this.currScene}));
-*/
-        if(sceneName.toLowerCase() == "ssceneend") {
+        */
+        if (sceneName.toLowerCase() == "ssceneend") {
             this.tutorComplete();
         }
         else {
             //this.updateUserPackage();
         }
     }
-
 }
 
 
